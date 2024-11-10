a973ff966a6013c22a4d12aabf1130ba1432ba28
fidasgabriel <gabriel.fidalgo@germinare.org.br>
2024-11-09 22:21:47 -0300

    removing dashboard

--- src/app/(private)/home/page.tsx
+++ src/app/(private)/home/page.tsx
@@ -9,27 +9,29 @@ import { getRecentResearchs } from "@/services/get-recent-researches";
 import { getUser } from "@/services/get-user";
 
 async function HomePage() {
-    const user = await getUser();
-    const recentResearches = await getRecentResearchs();
-    const tabs = [
-        { name: "Bem-Vindo", path: "/home", icon: <Home size={21} /> },
-        { name: "Bases", path: "/importacoes", icon: <Database size={21} /> },
-        { name: "Pesquisas", path: "/formularios", icon: <LucideBarChart2 size={21} /> },
-        { name: "Dashboard", path: "/dashboard", icon: <LucideBarChart2 size={21} /> },
-    ];
+  const user = await getUser();
+  const recentResearches = await getRecentResearchs();
+  const tabs = [
+    { name: "Bem-Vindo", path: "/home", icon: <Home size={21} /> },
+    { name: "Bases", path: "/importacoes", icon: <Database size={21} /> },
+    {
+      name: "Pesquisas",
+      path: "/formularios",
+      icon: <LucideBarChart2 size={21} />,
+    },
+    // { name: "Dashboard", path: "/dashboard", icon: <LucideBarChart2 size={21} /> },
+  ];
 
-    return (
-        <div>
-
-            <aside className="w-[300px] min-h-screen max-h-screen fixed overflow-scroll flex flex-col gap-6 p-8 shadow-xl scroll-hidden pb-[170px]">
-
-                <section className="flex items-center gap-4">
-                    <img width={40} height={40} src={user.company.logo} alt="" />
-                    <div>
-                        <p className="text-md font-semibold">{user.company.name}</p>
-                        <p className="text-sm text-insightfy-dark-gray">Seus Fluxos</p>
-                    </div>
-                </section>
+  return (
+    <div>
+      <aside className="w-[300px] min-h-screen max-h-screen fixed overflow-scroll flex flex-col gap-6 p-8 shadow-xl scroll-hidden pb-[170px]">
+        <section className="flex items-center gap-4">
+          <img width={40} height={40} src={user.company.logo} alt="" />
+          <div>
+            <p className="text-md font-semibold">{user.company.name}</p>
+            <p className="text-sm text-insightfy-dark-gray">Seus Fluxos</p>
+          </div>
+        </section>
 
         <div className="flex flex-col gap-2">
           {tabs.map(({ name, path, icon }, index) => (
