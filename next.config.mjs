680fcafd9c284e1163e5980c3c01b7e293d445a1
H-Leone <henriqueleone.dev@gmail.com>
2024-11-12 10:42:42 -0300

    final final

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env: {
-        API_URL: 'http://localhost:8080/v1/'
+        API_URL: 'https://back-vugt.onrender.com/v1/'
     }
 };
 
